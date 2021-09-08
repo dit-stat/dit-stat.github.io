155f8041cd590a5cdc30fb73d59140babd6f979d
Emidio Capriotti <emidio.capriotti@gmail.com>
2021-09-09 01:12:33 +0200

    update georss

--- progs/extract-data.sh
+++ progs/extract-data.sh
@@ -9,5 +9,5 @@ done
 for i in `seq 5 5 110`;
 do
    b=`echo $i-5|bc`;
-   python ./d-istat.py $1 42  -r Italy  --em 5 --sa $b  --ea $i |tail -n +2 |awk -v OFS=","  -v a=$i '{for (i=3;i<=12;i++) {print $1,$2,$i,2011+i-2,a}}' |grep -v SUM;
+   python ./d-istat.py $1 42  -r Italy  --em 6 --sa $b  --ea $i |tail -n +2 |awk -v OFS=","  -v a=$i '{for (i=12;i<=12;i++) {print $1,$2,$i,2011+i-2,a}}' |grep -v SUM;
 done
